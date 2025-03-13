e5596f2e0dbda598b2a7896a0e89890622facd06
HenL188 <153272326+HenL188@users.noreply.github.com>
2025-03-13 16:48:15 -0600

    improved build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,7 @@
-$build = $args[0]
+$build1 = $args[0]
+$build2 = $args[1]
+$build3 = $args[2]
+$build4 = $args[3]
 
 function main.cpp {
     if (Test-Path -Path "main.o"){
@@ -47,25 +50,23 @@ function build {
     menu.cpp
     scene.cpp
     game.cpp
-    link
-    run
 }
 
-if ($build -eq "main") {
+if ($build1 -eq "main" -or $build2 -eq "main" -or $build3 -eq "main" -or $build4 -eq "main") {
     main.cpp
-    link
-    run
 }
-elseif ($build -eq "menu") {
+if ($build1 -eq "menu" -or $build2 -eq "menu" -or $build3 -eq "menu" -or $build4 -eq "menu") {
     menu.cpp
-    link
-    run
 }
-elseif ($build -eq "scene"){
+if ($build1 -eq "scene" -or $build2 -eq "scene" -or $build3 -eq "scene" -or $build4 -eq "scene"){
     scene.cpp
-    link
-    run
 }
-else {
- build   
+if ($build1 -eq "game" -or $build2 -eq "game" -or $build3 -eq "game" -or $build4 -eq "game"){
+    game.cpp
 }
+if ($build1 -eq $null) {
+    build   
+}
+
+link
+run
